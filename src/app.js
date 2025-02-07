522a1d9ef832c0ef76a5a07e52a0951e4ba1544a
Jose Sancho <jose.sancho89@gmail.com>
2025-02-07 05:54:01 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let extensions = [".com", ".net", ".us", ".io"];
+
+  let combinations = [];
+
+  for (let a of pronoun) {
+    for (let b of adj) {
+      for (let c of noun) {
+        for(let d of extensions){
+          console.log(`${a}${b}${c}${d}`);
+        }
+      }
+    }
+  }
+
   console.log("Hello Rigo from the console!");
 };
